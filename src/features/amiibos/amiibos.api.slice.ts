7c5eaf74058af36193c2879a5505371154a65fdd
Luis Arancibia <l.arancibiaf@gmail.com>
2022-11-04 05:31:22 -0300

    done

--- src/features/amiibos/amiibos.api.slice.ts
+++ src/features/amiibos/amiibos.api.slice.ts
@@ -1,18 +1,47 @@
 import type IAmiibo from './amiibos.interface'
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
+import type { BaseQueryFn } from '@reduxjs/toolkit/query'
+import axios from 'axios'
+import type { AxiosRequestConfig, AxiosError } from 'axios'
+
+const axiosBaseQuery =
+  (
+    { baseUrl }: { baseUrl: string } = { baseUrl: '' }
+  ): BaseQueryFn<
+    {
+      url: string
+      method: AxiosRequestConfig['method']
+      data?: AxiosRequestConfig['data']
+      params?: AxiosRequestConfig['params']
+    },
+    unknown,
+    unknown
+  > =>
+  async ({ url, method, data, params }) => {
+    try {
+      const result = await axios({ url: baseUrl + url, method, data, params })
+      return { data: result.data }
+    } catch (axiosError) {
+      let err = axiosError as AxiosError
+      return {
+        error: {
+          status: err.response?.status,
+          data: err.response?.data || err.message
+        }
+      }
+    }
+  }
+
 export const apiSlice = createApi({
   reducerPath: 'amiibos',
 
-  baseQuery: fetchBaseQuery({
+  baseQuery: axiosBaseQuery({
     baseUrl: 'https://www.amiiboapi.com/api/'
   }),
   endpoints(builder) {
     return {
-      fetchAmiibos: builder.query<
-        IAmiibo[],
-        { search?: string; type?: string; gameseries?: string } | void
-      >({
+      fetchAmiibos: builder.query({
         query: (params) => {
           console.log(params)
           const urlParams = new URLSearchParams()
@@ -29,14 +58,14 @@ export const apiSlice = createApi({
               urlParams.set('name', params.search)
             }
           }
-          return `/amiibo?${urlParams.toString()}`
+          return { url: `/amiibo?${urlParams.toString()}`, method: "GET" }
         },
         transformResponse: (response: { amiibo: IAmiibo[] }) =>
           response.amiibo.map((amiibo) => ({
             ...amiibo,
             price: Math.floor(Math.random() * 1000) + 1,
             id: amiibo.head + amiibo.tail
-          })),
+          }))
       })
     }
   }
