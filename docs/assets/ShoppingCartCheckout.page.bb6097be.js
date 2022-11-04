c74ba97c39b15eb76b88a1ef76a10e8996783afa
Luis Arancibia <l.arancibiaf@gmail.com>
2022-11-04 05:14:52 -0300

    done

--- docs/assets/ShoppingCartCheckout.page.bb6097be.js
+++
@@ -1,1 +0,0 @@
-import{a as e,S as a}from"./index.af9cfd3e.js";const r=()=>e("section",{className:"w-[50%] flex flex-row mx-auto",children:e(a,{})});export{r as default};
--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,6 @@ import { defineConfig } from 'vite'
 console.log(process.env.NODE_ENV)
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:
-    process.env.NODE_ENV === 'production'
-      ? '/drakefood-frontend-application/'
-      : '/',
   plugins: [React()],
   resolve: {
     alias: {
